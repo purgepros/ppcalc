a7ae84dab9247c6259e8a8f1ff4a58d0319b9509
purgepros <matt@itspurgepros.com>
2025-10-30 19:16:02 -0400

    more dumb shit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: {
-        '@tailwindcss/postcss': {},
-        'autoprefixer': {},
-      },
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
     },
   },
 })
+
