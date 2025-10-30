d41586fe792cb585ff3e844fa48b86f473313c41
purgepros <matt@itspurgepros.com>
2025-10-30 19:06:33 -0400

    Manually add tailwind and postcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This tells it to scan all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
