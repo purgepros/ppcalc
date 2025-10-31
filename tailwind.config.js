2b71a59392a422865b6f2b02aea175445e03eaa0
purgepros <matt@itspurgepros.com>
2025-10-30 20:00:34 -0400

    dumb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // This scans your project
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // ADD THIS SAFELIST BLOCK
+  safelist: [
+    'bg-opacity-50', // Force this class to be included
+    'bg-opacity-25', // Might as well add this one too
   ],
   theme: {
     extend: {},
