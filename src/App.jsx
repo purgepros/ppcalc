3f41b10eeb7fa433dc929bc9e354e78dd390ed26
purgepros <matt@itspurgepros.com>
2025-11-16 21:05:01 -0500

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -243,8 +243,8 @@ const Sorter = ({ onSortComplete, onBack, initialYardSize, initialDogCount, dogF
       <div className="bg-green-100 border-l-4 border-green-500 text-green-900 p-4 rounded-r-lg mb-6 shadow-md flex items-center space-x-3 special-offer-glow">
         <svg className="w-8 h-8 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
         <div>
-          <p className="font-bold text-lg">{specialOffer.title}</p>
-          <p className="text-sm font-semibold" dangerouslySetInnerHTML={{ __html: specialOffer.body }} />
+          <p className="font-bold text-lg">{specialOffer.specialOfferTitle}</p>
+          <p className="text-sm font-semibold" dangerouslySetInnerHTML={{ __html: specialOffer.specialOfferBody }} />
         </div>
       </div>
 
@@ -307,8 +307,8 @@ const PackageSelector = ({ basePrices, planDetails, dogFee, dogCount, onPlanSele
       <div className="bg-green-100 border-l-4 border-green-500 text-green-900 p-4 rounded-r-lg mb-6 shadow-md flex items-center space-x-3 special-offer-glow">
         <svg className="w-8 h-8 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
         <div>
-          <p className="font-bold text-lg">{specialOffer.title}</p>
-          <p className="text-sm font-semibold" dangerouslySetInnerHTML={{ __html: specialOffer.body }} />
+          <p className="font-bold text-lg">{specialOffer.specialOfferTitle}</p>
+          <p className="text-sm font-semibold" dangerouslySetInnerHTML={{ __html: specialOffer.specialOfferBody }} />
         </div>
       </div>
 
