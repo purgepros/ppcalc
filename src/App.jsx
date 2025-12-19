9c6255e903970c7485de67696abba733bb6846f5
purgepros <matt@itspurgepros.com>
2025-12-19 01:14:58 -0500

    adjust checkout verbiage and calculation of savings placement

--- src/App.jsx
+++ src/App.jsx
@@ -1082,6 +1082,23 @@ const CheckoutForm = ({ packageSelection, initialPaymentSelection, zipCode, dogC
         <div className="flex justify-between mb-1 text-slate-500 italic"><span className="">Breakdown:</span><span className="">${bd.perVisit.toFixed(2)} / visit</span></div>
 
         <div className="border-t border-slate-200 my-2 pt-2">
+           {/* --- NEW ADDITION: First Cleanup Line Items --- */}
+           <div className="flex justify-between text-slate-500 mb-1">
+             <span>First Clean Up / Yard Reset</span>
+             <span className="text-slate-900">$99.99+</span>
+           </div>
+           <div className="flex justify-between text-green-600 font-bold mb-1 items-center">
+             <div className="flex items-center">
+               <span>FREE First Clean Up / Yard Reset</span>
+               <div className="flex items-center ml-1">
+                  <button onClick={onSavingsInfoClick} className="text-green-600 hover:text-green-800 focus:outline-none"><svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" /></svg></button>
+                  <button onClick={onSavingsInfoClick}><ClickHint/></button>
+               </div>
+             </div>
+             <span>-$99.99+</span>
+           </div>
+           {/* ---------------------------------------------- */}
+
            <div className="flex justify-between font-bold text-slate-800 mb-1">
              <span>Monthly Rate:</span>
              <span>${packageSelection.finalMonthlyPrice.toFixed(2)}</span>
